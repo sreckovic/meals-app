f14ad65c0303e030e3b0e6bdfb67f5785393e3cd
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2019-09-13 16:25:42 +0200

    fonts

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'Open Sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'Open Sans Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={setFontLoaded(true)} />;
+  }
+
+  return <MealsNavigator />;
 }
 
 const styles = StyleSheet.create({
