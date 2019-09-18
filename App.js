09c7af6a02721486a4b81bacee86d8838740670c
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2019-09-18 16:08:46 +0200

    init redux

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { useScreens } from 'react-native-screens';
 
-useScreens();
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import mealsReducer from './store/reducers/meals';
 
 import MealsNavigator from './navigation/MealsNavigator';
+
+useScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -29,14 +38,9 @@ export default function App() {
     );
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
